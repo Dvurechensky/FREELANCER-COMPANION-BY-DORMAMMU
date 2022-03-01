ce29319a4cf65fa3359ce3f2250c92d6ec62bff3
Dormammu <saomuun@mail.ru>
2022-03-01 04:11:49 +0300

    companion

---
+++ Freelancer Companion by Dormammu/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Freelancer_Companion_by_Dormammu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBoxSystems = new System.Windows.Forms.ComboBox();
+            this.labelSystemss = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxStatus.Location = new System.Drawing.Point(12, 4);
+            this.textBoxStatus.Multiline = true;
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxStatus.Size = new System.Drawing.Size(482, 58);
+            this.textBoxStatus.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 1000);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // comboBoxSystems
+            // 
+            this.comboBoxSystems.FormattingEnabled = true;
+            this.comboBoxSystems.Location = new System.Drawing.Point(701, 12);
+            this.comboBoxSystems.Name = "comboBoxSystems";
+            this.comboBoxSystems.Size = new System.Drawing.Size(311, 21);
+            this.comboBoxSystems.TabIndex = 2;
+            // 
+            // labelSystemss
+            // 
+            this.labelSystemss.AutoSize = true;
+            this.labelSystemss.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSystemss.Location = new System.Drawing.Point(513, 8);
+            this.labelSystemss.Name = "labelSystemss";
+            this.labelSystemss.Size = new System.Drawing.Size(117, 28);
+            this.labelSystemss.TabIndex = 3;
+            this.labelSystemss.Text = "CИСТЕМ: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1031, 1080);
+            this.Controls.Add(this.labelSystemss);
+            this.Controls.Add(this.comboBoxSystems);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxStatus);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Freelancer Companion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxSystems;
+        private System.Windows.Forms.Label labelSystemss;
+    }
+}
+
---
+++ Freelancer Companion by Dormammu/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Freelancer_Companion_by_Dormammu
+{
+    public partial class Form1 : Form
+    {
+        private List<string> Systems = new List<string>();
+        private int Systemss;
+        //Пикселей в одном делении оси
+        const int PIX_IN_ONE = 5;
+        //Длина стрелки
+        const int ARR_LEN = 3;
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)]string lpFileName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int LoadString(IntPtr hInstance, int ID, StringBuilder lpBuffer, int nBufferMax);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FreeLibrary(IntPtr hModule);
+
+        private string ExtractStringFromDLL(string file, int number)
+        {
+            IntPtr lib = LoadLibrary(file);
+            StringBuilder result = new StringBuilder(2048);
+            LoadString(lib, number, result, result.Capacity);
+            FreeLibrary(lib);
+            return result.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFile();
+            comboBoxSystems.SelectedIndex = 0;
+            CheckDataInform();
+        }
+
+        private void CheckDataInform()
+        {
+            //Чищу данные систем от зон
+            List<List<string>> sys = new List<List<string>>();
+        }
+
+        private void InitializeFile()
+        {
+            string loadedString = ExtractStringFromDLL("NameResources.dll", 0);
+            textBoxStatus.AppendText(loadedString);
+
+            var pathsystems = System.IO.Directory.GetDirectories("SYSTEMS").Length;
+            DirectoryInfo dinfo = new DirectoryInfo("SYSTEMS");
+            DirectoryInfo[] dirfos = dinfo.GetDirectories();
+                
+            foreach (DirectoryInfo dirnames in dirfos)
+            {
+                Systems.Add(dirnames.ToString());
+                comboBoxSystems.Items.Add(dirnames.ToString());
+            }
+            textBoxStatus.AppendText("Число систем определено!" + Environment.NewLine);
+
+            labelSystemss.Text = "CИСТЕМ: " + comboBoxSystems.Items.Count.ToString();
+        }
+
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int w = pictureBox1.ClientSize.Width / 2;
+            int h = pictureBox1.ClientSize.Height / 2;
+            //Смещение начала координат в центр PictureBox
+            e.Graphics.TranslateTransform(w, h);
+            DrawXAxis(new Point(-w, 0), new Point(w, 0), e.Graphics);
+            DrawYAxis(new Point(0, h), new Point(0, -h), e.Graphics);
+            //Центр координат
+            e.Graphics.FillEllipse(Brushes.Red, -2, -2, 4, 4);
+        }
+
+        //Рисование оси X
+        private void DrawXAxis(Point start, Point end, Graphics g)
+        {
+            //Деления в положительном направлении оси
+            for (int i = PIX_IN_ONE; i < end.X - ARR_LEN; i += PIX_IN_ONE)
+            {
+                g.DrawLine(Pens.Black, i, -1, i, 1);
+            }
+            //Деления в отрицательном направлении оси
+            for (int i = -PIX_IN_ONE; i > start.X; i -= PIX_IN_ONE)
+            {
+                g.DrawLine(Pens.Black, i, -1, i, 1);
+            }
+            //Ось
+            g.DrawLine(Pens.Black, start, end);
+            //Стрелка
+            g.DrawLines(Pens.Black, GetArrow(start.X, start.Y, end.X, end.Y, ARR_LEN));
+        }
+
+        //Рисование оси Y
+        private void DrawYAxis(Point start, Point end, Graphics g)
+        {
+            //Деления в отрицательном направлении оси
+            for (int i = PIX_IN_ONE; i < start.Y; i += PIX_IN_ONE)
+            {
+                g.DrawLine(Pens.Black, -1, i, 1, i);
+            }
+            //Деления в положительном направлении оси
+            for (int i = -PIX_IN_ONE; i > end.Y + ARR_LEN; i -= PIX_IN_ONE)
+            {
+                g.DrawLine(Pens.Black, -1, i, 1, i);
+            }
+            //Ось
+            g.DrawLine(Pens.Black, start, end);
+            //Стрелка
+            g.DrawLines(Pens.Black, GetArrow(start.X, start.Y, end.X, end.Y, ARR_LEN));
+        }
+
+        //Рисование текста
+        private void DrawText(Point point, string text, Graphics g, bool isYAxis = false)
+        {
+            var f = new Font(Font.FontFamily, 6);
+            var size = g.MeasureString(text, f);
+            var pt = isYAxis
+                ? new PointF(point.X + 1, point.Y - size.Height / 2)
+                : new PointF(point.X - size.Width / 2, point.Y + 1);
+            var rect = new RectangleF(pt, size);
+            g.DrawString(text, f, Brushes.Black, rect);
+        }
+
+        //Вычисление стрелки оси
+        private static PointF[] GetArrow(float x1, float y1, float x2, float y2, float len = 10, float width = 4)
+        {
+            PointF[] result = new PointF[3];
+            //направляющий вектор отрезка
+            var n = new PointF(x2 - x1, y2 - y1);
+            //Длина отрезка
+            var l = (float)Math.Sqrt(n.X * n.X + n.Y * n.Y);
+            //Единичный вектор
+            var v1 = new PointF(n.X / l, n.Y / l);
+            //Длина стрелки
+            n.X = x2 - v1.X * len;
+            n.Y = y2 - v1.Y * len;
+            result[0] = new PointF(n.X + v1.Y * width, n.Y - v1.X * width);
+            result[1] = new PointF(x2, y2);
+            result[2] = new PointF(n.X - v1.Y * width, n.Y + v1.X * width);
+            return result;
+        }
+    }
+}
